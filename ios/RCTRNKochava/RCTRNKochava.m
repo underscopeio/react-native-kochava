70b20088772a1049be42bbc77de0309fc1d75404
Ignacio Olaciregui <ignacioola@gmail.com>
2018-06-01 18:46:28 -0300

    [OMD-320] Fix kochava ios crash

--- ios/RCTRNKochava/RCTRNKochava.m
+++ ios/RCTRNKochava/RCTRNKochava.m
@@ -5,28 +5,26 @@
 
 RCT_EXPORT_MODULE();
 
-RCT_REMAP_METHOD(init,
-                 options: (NSDictionary *)options
-                 resolver:(RCTPromiseResolveBlock)resolve
-                 rejecter:(RCTPromiseRejectBlock)reject)
-{
-    BOOL *enableDebug = [RCTConvert BOOL:options[@"enableDebug"]];
-    NSString *appId = [RCTConvert NSString:options[@"appId"]];
-{
-    dispatch_async(dispatch_get_main_queue(), ^{
-        NSMutableDictionary *trackerParametersDictionary = [NSMutableDictionary dictionary];
-        trackerParametersDictionary[kKVAParamAppGUIDStringKey] = appId;
-        if (enableDebug) {
-            trackerParametersDictionary[kKVAParamLogLevelEnumKey] = kKVALogLevelEnumDebug;
-        }
-        [KochavaTracker.shared configureWithParametersDictionary:trackerParametersDictionary delegate:self];
-    });
- 
-}
+//RCT_REMAP_METHOD(init,
+//                 options: (NSDictionary *)options
+//                 resolver:(RCTPromiseResolveBlock)resolve
+//                 rejecter:(RCTPromiseRejectBlock)reject)
+//{
+//    BOOL *enableDebug = [RCTConvert BOOL:options[@"enableDebug"]];
+//    NSString *appId = [RCTConvert NSString:options[@"appId"]];
+//    dispatch_async(dispatch_get_main_queue(), ^{
+//        NSMutableDictionary *trackerParametersDictionary = [NSMutableDictionary dictionary];
+//        trackerParametersDictionary[kKVAParamAppGUIDStringKey] = appId;
+//        if (enableDebug) {
+//            trackerParametersDictionary[kKVAParamLogLevelEnumKey] = kKVALogLevelEnumDebug;
+//        }
+//        [KochavaTracker.shared configureWithParametersDictionary:trackerParametersDictionary delegate:self];
+//    });
+//}
 
 RCT_EXPORT_METHOD(identityLink:(NSDictionary *)identityLinkDictionary
-                 resolver:(RCTPromiseResolveBlock)resolve
-                 rejecter:(RCTPromiseRejectBlock)reject)
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 {
     [KochavaTracker.shared sendIdentityLinkWithDictionary:identityLinkDictionary];
     resolve(@"");
